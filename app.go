20fd8cd013cb252fc5af6cdfa5a1a7a48119a74b
Maxime Lesbros <mlazzje@users.noreply.github.com>
2016-05-23 09:46:49 +0200

    Update app.go

--- app.go
+++ app.go
@@ -1,16 +1,10 @@
 package app
 
 import (
-    //"fmt"
     "net/http"
 )
 
 func init() {
-    //http.HandleFunc("/", handler)
     fs := http.FileServer(http.Dir("view"))
     http.Handle("/", fs)
 }
-
-/*func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
-}*/
